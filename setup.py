ff65b029d2f22f803218a9892e0d944e21f17aa5
Alex Rothberg <agrothberg@gmail.com>
2023-06-19 17:49:49 -0400

    Add Python 3.11 classifier (#296)

--- setup.py
+++ setup.py
@@ -125,6 +125,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     # $ setup.py publish support.
